521560f15dddd9a79386796bdd8c36a8161c9a40
rattus-stoicus <198841310+rattus-stoicus@users.noreply.github.com>
2026-08-30 19:34:18 -0500

    fix(zsh): fix error on config for mac os

--- .config/zsh.d/40-themes.zsh
+++ .config/zsh.d/40-themes.zsh
@@ -1,6 +1,8 @@
+OS_ICON=""
+
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    OS_ICON = "%F{white}%f"
-elif grep -q 'Arch' /etc/os-release; then
+    OS_ICON="%F{white}%f"
+elif grep -q 'Arch' /etc/os-release 2>/dev/null; then
     OS_ICON="%F{cyan}󰣇%f"
 else
     OS_ICON="%F{white}%f"
