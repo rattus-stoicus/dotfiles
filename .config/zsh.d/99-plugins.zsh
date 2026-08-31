e825f929fbbe7bd355c13a2cafac36acb3162dcb
rattus-stoicus <198841310+rattus-stoicus@users.noreply.github.com>
2026-08-30 19:51:23 -0500

    feat(zsh): add support for plugins on mac os

--- .config/zsh.d/99-plugins.zsh
+++ .config/zsh.d/99-plugins.zsh
@@ -1,2 +1,12 @@
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+PLUGIN_ROOT=""
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    PLUGIN_ROOT="$(brew --prefix)/share"
+else
+    PLUGIN_ROOT="/usr/share/zsh/plugins"
+fi
+
+highlight="${PLUGIN_ROOT}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+autosuggest="${PLUGIN_ROOT}/zsh-autosuggestions/zsh-autosuggestions.zsh"
+
+[[ -f "$autosuggest" ]] && source "$autosuggest"
+[[ -f "$highlight" ]] && source "$highlight"
