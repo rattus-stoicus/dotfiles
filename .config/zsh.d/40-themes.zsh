3ec544af55e01b7a5011995dfeb1a542fbf695b4
rattus-stoicus <198841310+rattus-stoicus@users.noreply.github.com>
2026-08-30 19:11:30 -0500

    feat(zsh): add custom icon based on OS

--- .config/zsh.d/40-themes.zsh
+++ .config/zsh.d/40-themes.zsh
@@ -1,1 +1,9 @@
-PROMPT='󰣇  %~ %# '
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS_ICON = "%F{white}%f"
+elif grep -q 'Arch' /etc/os-release; then
+    OS_ICON="%F{cyan}󰣇%f"
+else
+    OS_ICON="%F{white}%f"
+fi
+
+PROMPT="${OS_ICON} %~ %# "
