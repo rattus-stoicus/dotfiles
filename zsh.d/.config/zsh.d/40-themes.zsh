5719e396c6b62a811de726ae9ea30392aca5136c
punktelia <198841310+punktelia@users.noreply.github.com>
2026-09-01 20:52:27 -0500

    chore(zsh): add colors to mac os prompt

--- zsh.d/.config/zsh.d/40-themes.zsh
+++ zsh.d/.config/zsh.d/40-themes.zsh
@@ -1,11 +1,15 @@
 OS_ICON=""
+COLOR_DIRS="white"
+COLOR_PROMPT="white"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    OS_ICON="%F{white}%f"
+    OS_ICON="%F{#FFF8DC}%f"
+    COLOR_DIRS="#EB5A3C"
+    COLOR_PROMPT="#F7C566"
 elif grep -q 'Arch' /etc/os-release 2>/dev/null; then
     OS_ICON="%F{cyan}󰣇%f"
 else
     OS_ICON="%F{white}%f"
 fi
 
-PROMPT="${OS_ICON} %~ %# "
+PROMPT="${OS_ICON} %F{${COLOR_DIRS}}%~%f %F{${COLOR_PROMPT}}%# :%f "
